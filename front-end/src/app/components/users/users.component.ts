4a620005e9cd4fa419ed1836ce73a93e63a85cd2
Alper Dokay <aodokay@gmail.com>
2021-05-16 23:11:33 +0300

    user service completed alongside its models

--- front-end/src/app/components/users/users.component.ts
+++ front-end/src/app/components/users/users.component.ts
@@ -26,75 +26,10 @@ export class UsersComponent implements OnInit {
     private roleService: RoleService,
     private apiService: GeneralApiService
   ) {
-    // this.roleService.getRoles().subscribe(response => {
-    //   if (response.success) {
-    //     this.roles = response.data;
-    //   }
-    // })
   }
 
   ngOnInit(): void {
-    this.loadingService.setLoading(true);
-    this.getUsers();
+    
   }
 
-  getUsers() {
-    this.userService.getUsers().subscribe(response => {
-      if (response.success) {
-        console.log(response);
-        this.users = response.data;
-      }
-      else {
-        this.alertService.alert('Hata!', 'Kullanıcılar alınamadı.', 'error');
-      }
-      this.loadingService.setLoading(false);
-    });
-  }
-
-  getRoleName(roleId: string) {
-    const role = this.roles.find(x => x.id === roleId);
-    return role ? role.roleName : '-';
-  }
-
-  showChangePasswordModal(user: User) {
-    this.selectedUserId = user.id;
-    $('.modal').modal('show')
-  }
-
-  hideChangePasswordModal() {
-    $('.modal').modal('hide')
-    this.newPasswordBody = {
-      password: '',
-      confirm: ''
-    }
-    this.selectedUserId = null;
-  }
-
-  changePassword() {
-    if (this.newPasswordBody.password !== this.newPasswordBody.confirm) {
-      return this.alertService.alert('Hata!', 'Şifreler uyuşmuyor', 'error');
-    }
-    this.userService.changePassword({
-      password: this.newPasswordBody.password,
-      userId: this.selectedUserId
-    }).subscribe(response => {
-      if (response.success) {
-        this.alertService.notification('Başarılı!', 'Şifre başarıyla güncellendi', 'success');
-        this.hideChangePasswordModal();
-      } else {
-        this.alertService.notification('Hata!', 'Şifre güncellenemedi', 'error');
-      }
-    })
-  }
-
-  deleteUser(id: string, email: string) {
-    const x = confirm(email + ' kullanıcısını silmek istediğinize emin misiniz?');
-    if (x) {
-      this.apiService.deleteRecord(id, 'user').subscribe(response => {
-        if (response.success) {
-          this.getUsers();
-        }
-      })
-    }
-  }
 }
