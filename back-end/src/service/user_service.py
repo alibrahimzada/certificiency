e364782ba969111cc548d1078ee6a81afac11eca
alibrahimzada <alibrahimzada@gmail.com>
2021-05-21 00:10:04 +0300

    added encryption for user insert

--- back-end/src/service/user_service.py
+++ back-end/src/service/user_service.py
@@ -1,10 +1,12 @@
 from src.service import Service
 from src.data.models.user import User
+from src.service.helpers.crypto_helper import CryptoHelper
 
 class UserService(Service):
 
     def __init__(self):
         self.user = User()
+        self.crypto_helper = CryptoHelper()
 
     def get_users(self):
         return self.user.get_all_users()
@@ -13,6 +15,8 @@ class UserService(Service):
         return self.user.get_user(user_id)
 
     def insert_user(self, data):
+        encrypted_password = self.encrypt_password(data)
+        data['password'] = encrypted_password
         return self.user.insert_user(data)
 
     def delete_user(self, data):
@@ -20,3 +24,7 @@ class UserService(Service):
 
     def update_user(self, data):
         return self.user.update_user(data)
+
+    def encrypt_password(self, data):
+        encrypted_password = self.crypto_helper.sha256_encrypt(data['username'], data['password'])
+        return encrypted_password
