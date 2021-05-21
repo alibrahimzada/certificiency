a01f0fb3b3bdafbcca64150ffa8cd5c63fa14eaa
alibrahimzada <alibrahimzada@gmail.com>
2021-05-21 14:28:55 +0300

    created JWT

--- back-end/src/service/auth_service.py
+++ back-end/src/service/auth_service.py
@@ -1,6 +1,16 @@
 from src.service import Service
 from src.data.models.auth import Auth 
 from src.service.helpers.crypto_helper import CryptoHelper
+from flask_jwt import JWT
+from werkzeug.security import safe_str_cmp
+from src.api.app import app
+
+class UserLogin():
+
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
 
 class AuthService(Service):
 
@@ -11,8 +21,28 @@ class AuthService(Service):
     def login(self, data):
         encrypted_password = self.encrypt_password(data)
         data['password'] = encrypted_password
-        return self.auth.login(data)
+
+        api_response = self.auth.login(data)
+        if api_response['success']:
+            user_id = api_response['data']['user_id']
+            user_name = api_response['data']['username']
+            password = api_response['data']['password']
+
+            user = UserLogin(user_id, user_name, password)
+
+            jwt = JWT(app, self.authenticate, self.identity)
+            
+        return api_response
 
     def encrypt_password(self, data):
         encrypted_password = self.crypto_helper.sha256_encrypt(data['username'], data['password'])
         return encrypted_password
+
+    def authenticate(self, username, password):
+        user = username_table.get(username, None)
+        if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+            return user
+
+    def identity(self, payload):
+        user_id = payload['identity']
+        return userid_table.get(user_id, None)
