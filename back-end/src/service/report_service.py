ebdc5334153facf6fa2c594f6e925851a053964b
beyzaaydogan <beyzaaydogan026@gmail.com>
2021-06-04 15:39:59 +0300

    added event, customer, user, event cat reports

--- back-end/src/service/report_service.py
+++ back-end/src/service/report_service.py
@@ -6,11 +6,14 @@ class ReportService(Service):
     def __init__(self):
         self.report = Report()
 
-    def get_event_report(self):
-        pass
+    def get_event_report(self, event_id):
+        return self.report.get_event_report(event_id)
 
-    def get_customer_report(self):
-        pass
+    def get_customer_report(self, customer_id):
+        return self.report.get_customer_report(customer_id)
 
-    def get_user_report(self):
-        pass
+    def get_event_category_report(self, event_category_id):
+        return self.report.get_event_category_report(event_category_id)
+
+    def get_user_report(self, user_id):
+        return self.report.get_user_report(user_id)
